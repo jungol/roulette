63ff0ec688e239fc4dcacf2a055ef6ad4718fe16
Ethan Barhydt <ethanbarhydt@gmail.com>
2014-10-26 01:06:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roulette::Application.config.secret_key_base = 'eb469433a95e970a38da81b44ebba69f189e2044fbd56510896675fb8978d2e93e738853f05ef7800bfac56e9ad7ac2aca07d4b4cca30e3899d7ef24fa688aa4'
