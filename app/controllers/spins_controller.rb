6dc86a887521b2ea77fe7da5c702235723f5db8c
Ethan Barhydt <ethanbarhydt@gmail.com>
2014-10-26 22:42:53 -0400

    print locations after a spin

--- app/controllers/spins_controller.rb
+++ app/controllers/spins_controller.rb
@@ -1,7 +1,7 @@
 class SpinsController < ApplicationController
   
   def new
-    
+    @spots = []
   end
 
   def create
@@ -9,12 +9,12 @@ class SpinsController < ApplicationController
     @client = GooglePlaces::Client.new('AIzaSyB_QWi7zPKtb69Nl11UJoFUsaxqZZztYWk')
     @spots = @client.spots(location.latitude, location.longitude, :types => 'restaurant')
     # methods are: location.latitude, location.longitude
+    # @spots = @client.spots(51.5033630, -0.1276250, :types => 'restaurant')    
     
     # find restaurant by location
     # @restaurant = [ restaurant ]
-    
-    redirect_to root_path 
+    @spot = "hey"
+    render 'new' 
   end
 
 end
-
